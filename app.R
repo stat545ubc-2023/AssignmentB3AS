df0659b6db4d9e533992d1d57e776a3d633e2599
andrasszeitz <andras.szeitz@ubc.ca>
2023-11-21 15:34:54 -0800

    committ

---
+++ app.R
@@ -0,0 +1,4 @@
+install.packages('rsconnect')
+rsconnect::setAccountInfo(name='assignmentb3as',
+                          token='C25B34C2719C95CFA2980D0E50E81573',
+                          secret='LVPPgYSfftG9+v5OpP+oFk4MCr2aBLlQ0zOO9hYP')
